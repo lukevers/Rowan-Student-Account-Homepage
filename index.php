2cb5d416ba0d5aa2db858d2b7e835cddd1c9287e
Luke Evers <me@lukevers.com>
2013-01-30 13:28:21 -0500

    Initial Commit.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Luke Evers</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="center">
+			ohai
+		</div>
+	</body>
+</html>
