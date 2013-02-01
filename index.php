8299717ba9dd8a18b518dbe321cf88bc1a67abef
Luke Evers <me@lukevers.com>
2013-02-01 10:40:50 -0500

    Added another font for the main text.
    Also updated some style changes.
    Also started to write a short description.
    Also started to put a few links up.

--- index.php
+++ index.php
@@ -6,7 +6,18 @@
 	</head>
 	<body>
 		<div class="center">
-			Luke Evers
+			<div class="name">
+            	Luke Evers
+            </div>
+            <div class="desc">
+            	OH HELLO! LOOKS LIKE YOU'VE FOUND MY ROWAN PAGE. 
+                I PROBABLY WON'T UPDATE THIS MUCH, SO VISIT ANY
+                OF THE FOLLOWING LINKS!
+            </div>
+            <a href="http://lukevers.com/" class="button">WEBSITE</a>
+            <a href="https://github.com/lukevers" class="button">GITHUB</a>
+            <a href="" class="button">Button3</a>
+            <a href="" class="button">Button4</a>
 		</div>
 	</body>
 </html>
