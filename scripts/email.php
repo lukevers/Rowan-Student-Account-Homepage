2158d2e62e4b9dbabb8b4f982b4712c529c83c22
Luke Evers <me@lukevers.com>
2013-02-01 14:09:28 -0500

    PHP testing.

--- scripts/email.php
+++ scripts/email.php
@@ -1,5 +1,17 @@
 <?php
 	
-	echo 'derp';
+	$to = "me@lukevers.com";
+	$from = $_POST["email"];
+	$subject = $_POST["subject"];
+	$body = $_POST["body"];
+	
+	$headers = "From: ".$from;
+	
+	echo $to;
+	echo $subject;
+	echo $body;
+	echo $headers;
+	
+	//mail($to, $subject, $body, $headers);
 	
 ?>
