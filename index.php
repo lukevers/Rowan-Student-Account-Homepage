3dfed8b0eaff1376ec883cd832c1644e896279cf
Luke Evers <me@lukevers.com>
2013-02-01 10:47:40 -0500

    Style changes.

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
                 I PROBABLY WON'T UPDATE THIS MUCH, SO VISIT ANY
                 OF THE FOLLOWING LINKS:
             </div>
-            <a href="http://lukevers.com/" class="button">WEBSITE</a>
-            <a href="https://github.com/lukevers" class="button">GITHUB</a>
-            <a href="mailto:me@lukevers.com" class="button">EMAIL</a>
+            <a href="http://lukevers.com/" class="button">WEBSITE</a> -
+            <a href="https://github.com/lukevers" class="button">GITHUB</a> -
+            <a href="mailto:me@lukevers.com" class="button">EMAIL</a> -
             <a href="https://maryland.projectmeshnet.org" class="button">MESHNET</a>
 		</div>
 	</body>
