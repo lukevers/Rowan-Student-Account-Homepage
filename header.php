5cba59cd9008c75e99923f703de7d9bef6623f5e
Luke Evers <me@lukevers.com>
2013-02-01 21:45:33 -0500

    Added a shortcut icon

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Luke Evers</title>
 		<link rel="stylesheet" href="http://elvis.rowan.edu/~eversl88/css/style.css">
+		<link rel="shortcut icon" href="http://elvis.rowan.edu/~eversl88/favicon.ico">
 	</head>
 	<body>
 		<div class="center">
